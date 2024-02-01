fe3f408217a028f2ef2ca2a0e4cec668cac68d9b
vrudhi <you@example.com>
2024-02-01 19:29:43 +0530

    first commit of c++

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+    int b=20;
+    int c=40;
+
+    cout << "value of sum :" << a+b+c;
+    return 0;
+}
---
+++ dynamic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    cout << "value of a:";
+    cin >> a;
+    cout << "value of b:";
+    cin >> b;
+    sum = a + b;
+    cout << "Sum of "<<a <<" + " <<b <<" = " <<sum;
+    return 0;
+}
